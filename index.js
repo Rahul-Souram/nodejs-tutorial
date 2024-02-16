b14aff308d8fe6376260353d79a0e5db7893a651
Rahul souram <souramrahul@gmail.com>
2024-02-16 23:32:41 +0530

    progress bar and color added

--- index.js
+++ index.js
@@ -35,3 +35,41 @@ const measureTime = () => {
 };
 
 measureTime();
+
+const ProgressBar = require("progress");
+
+// Options:
+
+// total total number of ticks to complete
+// width the displayed width of the progress bar defaulting to total
+// stream the output stream defaulting to stderr
+// complete completion character defaulting to "="
+// incomplete incomplete character defaulting to "-"
+// renderThrottle minimum time between updates in milliseconds defaulting to 16
+// callback optional function to call when the progress bar completes
+// clear will clear the progress bar upon termination
+
+// Tokens:
+
+// :bar the progress bar itself
+// :current current tick number
+// :total total ticks
+// :elapsed time elapsed in seconds
+// :percent completion percentage
+// :eta eta in seconds
+
+const bar = new ProgressBar("downloading [:bar] :rate/bps :percent :eta", {
+  total: 30,
+});
+
+const timer = setInterval(() => {
+  bar.tick();
+  if (bar.complete) {
+    clearInterval(timer);
+  }
+}, 100);
+
+//chalk to color
+const chalk = require("chalk");
+
+console.log(chalk.red("This is in red color"));
